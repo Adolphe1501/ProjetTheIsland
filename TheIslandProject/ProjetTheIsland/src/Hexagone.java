07ff3cf6b9f9daa8306ac17a2b8dc05c9ce4d3ea
elielnd <elnditar@gmail.com>
2022-05-01 21:57:30 +0200

    Amelioration Structure

--- TheIslandProject/ProjetTheIsland/src/Hexagone.java
+++ TheIslandProject/ProjetTheIsland/src/Hexagone.java
@@ -1,17 +1,31 @@
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.*;
 
-public class Hexagone {
 
-    public List<Bateau> liste_bateau;
-    public List<P_Joueur> liste_joueur;
-    public List<AnimalDeMer> liste_animaux;
+
+public class Hexagone extends Polygon
+{
+
+    private List<Bateau> liste_bateau;
+    private List<P_Joueur> liste_joueur;
+    private List<AnimalDeMer> liste_animaux;
+    private Boolean zone_ile; 
+
+    private int[] xPoints;
+    private int[] yPoints;
+    private static final int nPoints = 6;
+
     
-    public Hexagone() 
+    public Hexagone(int[] xPoints, int[] yPoints) 
     {
+        super(xPoints, yPoints, nPoints);
+
         this.liste_animaux = new ArrayList<AnimalDeMer>();
         this.liste_joueur = new ArrayList<P_Joueur>();
-        this.liste_bateau = new ArrayList<Bateau>();
+        this.liste_bateau = new ArrayList<Bateau>();        
+        this.zone_ile = false;
     }
 
     
@@ -58,6 +72,41 @@ public class Hexagone {
     public void setListe_animaux(List<AnimalDeMer> liste_animaux) 
     {
         this.liste_animaux = liste_animaux;
+    }
+
+
+    public Boolean getZone_ile() {
+        return zone_ile;
+    }
+
+
+    public void setZone_ile(Boolean zone_ile) {
+        this.zone_ile = zone_ile;
+    }
+
+
+    public int[] getxPoints() {
+        return xPoints;
+    }
+
+
+    public void setxPoints(int[] xPoints) {
+        this.xPoints = xPoints;
+    }
+
+
+    public int[] getyPoints() {
+        return yPoints;
+    }
+
+
+    public void setyPoints(int[] yPoints) {
+        this.yPoints = yPoints;
+    }
+
+
+    public static int getNpoints() {
+        return nPoints;
     }
 
 }
--- TheIslandProject/ProjetTheIsland/src/Plateau.java
+++ TheIslandProject/ProjetTheIsland/src/Plateau.java
@@ -1,18 +1,149 @@
-import java.awt.Color;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
+
 
 public class Plateau extends JPanel
 {
+    private static final int nombre_ligne = 13;
+    private static final int nombre_colonne = 12;
+
     public static Hexagone map[][];
     public Jeu partie;
 
     public Plateau() 
     {
         super();
-
-        this.setBackground(Color.ORANGE);
+        map = new Hexagone[nombre_ligne][nombre_colonne];
+        
     }
 
+    public void paintComponent(Graphics g)
+    {
+        Graphics g2D = (Graphics2D) g;
+
+        construirePlateau(g2D);
+    }
+
+    private void construirePlateau(Graphics g2D)
+    {
+        int decalageX = 40, decalageY = 0, k=0;
+        String mapCharge[] = chargerMap();
+
+        for(int i =0; i<nombre_ligne; i++)
+        {            
+            System.out.print(mapCharge[i]);
+    
+        }
+        
+
+        int[] xPoints = new int[6];
+        int[] yPoints = new int[6];
+
+        int[] debutX = {40, 80, 120, 120, 80, 40};
+        int[] debutY = {15, 5, 15, 55, 65, 55};
+
+
+        for(int i =0; i<nombre_ligne; i++)
+        {            
+            for(int j=0; j<nombre_colonne; j++)
+            {
+                for(int point=0; point<Hexagone.getNpoints(); point++)
+                {
+                    if(i%2==0)
+                    {
+                        xPoints[point] = decalageX + debutX[point] + j*80;
+                        yPoints[point] = decalageY + debutY[point] + k*100;
+                    }
+                    else
+                    {
+                        xPoints[point] = debutX[point] + j*80;
+                        yPoints[point] = debutY[point] + i*50;
+                    }
+                }
+
+                Hexagone hexagone = new Hexagone(xPoints, yPoints);
+                map[i][j] = hexagone;
+
+                g2D.setColor(Color.blue);
+                g2D.drawPolygon(hexagone);
+            }
+
+            if(i%2==0)
+            {
+                k++;
+            }
+        }
+    }
+
+    private boolean contientZoneIle(int i, int j)
+    {
+        int Ymin = 3, Ymax = 9;
+        int Xmin = 4, Xmax = 9;
+        boolean ile = false;
+
+        if(i>=Ymin && i<=Ymax) 
+        {
+            if(i==Ymin || i==Ymax)
+            {
+                if(j>Xmin && j<Xmax)
+                {
+                    ile = true;
+                }
+            }
+            else if(i==Ymin+1 || i==Ymax-1)
+            {
+                if(j>Xmin && j<Xmax+1)
+                {
+                    ile = true;
+                }
+            }
+            else if(i==Ymin+2 || i==Ymax-2)
+            {
+                if(j>Xmin-2 && j<Xmax+2)
+                {
+                    ile = true;
+                }
+            }
+            else if(i==6)
+            {
+                if(j>3 && j<=10)
+                {
+                    ile = true;
+                }
+            }           
+        }
+        return ile;
+    }
+    
+    private String[] chargerMap()
+    {
+        String map[] = new String[nombre_ligne];
+
+        try 
+        {
+            FileReader fichier = new FileReader("map.txt");
+
+            System.out.println("Erreur ****");
+            BufferedReader reader = new BufferedReader(fichier);
+
+            System.out.println("Erreur******************");
+            for(int i = 0; i<nombre_ligne; i++)
+            {
+                map[i] = reader.readLine();
+            }
+
+            reader.close();
+        } 
+        catch (Exception e1) 
+        {
+            e1.printStackTrace();
+            System.out.println("Erreur");
+        }
+        
+        return map;
+    }
 }
