0fd6ce8aa3fb0a54e35d6e0937679dffc719fb7c
elielnd <elnditar@gmail.com>
2022-05-01 13:15:54 +0200

    Organisation da la fenetre de jeu

--- TheIslandProject/ProjetTheIsland/src/Plateau.java
+++ TheIslandProject/ProjetTheIsland/src/Plateau.java
@@ -1,13 +1,18 @@
+import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class Plateau 
+public class Plateau extends JPanel
 {
     public static Hexagone map[][];
     public Jeu partie;
 
     public Plateau() 
     {
+        super();
 
+        this.setBackground(Color.ORANGE);
     }
 
 }
---
+++ TheIslandProject/ProjetTheIsland/src/ZoneJoueur.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ZoneJoueur extends JPanel
+{
+    public ZoneJoueur()
+    {
+        super();
+
+        this.setBackground(Color.DARK_GRAY);
+
+    }
+    
+}
