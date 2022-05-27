b36790b33bc899a3dd468c8adf20ac686f6ded19
Adolphe1501 <100210115+Adolphe1501@users.noreply.github.com>
2022-05-27 10:36:30 +0200

    mise à jour déplacement joueur

--- ProjetTheIsland-Eliel-3/src/P_Joueur.java
+++ ProjetTheIsland-Eliel-3/src/P_Joueur.java
@@ -177,24 +177,33 @@ public class P_Joueur extends JLabel implements MouseListener
                     {
                         if (((posD.getNumero_ligne() == posA.getNumero_ligne()) && (y == 2 || y == -2)) || ((posD.getNumero_colone() == posA.getNumero_colone()) && (x == 2 || x == -2)) || ((y < 2 && y > -2) && (x == 2 || x == -2)) ||  (posD.getNumero_ligne()%2==0 && (y == -1 ||  y == 2) && (x== 1 || x == -1)   ) ||   (posD.getNumero_ligne()%2!=0 && (y == -2 ||  y == 1) && (x==1 || x == -1))   )            
                         {
-                            this.deplacerPj(hexagoneDepart, hexagoneArrivee);
-                            this.joueur.setNombre_deplacement(this.joueur.getNombre_deplacement() - 2);
-                            deplacement= true;
-                            if ( hexagoneArrivee.getTuile() == null)
-                                this.est_nageur = true;
-                        
+                            if(hexagoneArrivee.getTuile() == null)
+                            {
+                                this.deplacerPj(hexagoneDepart, hexagoneArrivee);
+                                this.joueur.setNombre_deplacement(this.joueur.getNombre_deplacement() - 2);
+                                deplacement= true;
+                                if ( hexagoneArrivee.getTuile() == null)
+                                    this.est_nageur = true;
+                            
+                            }else
+                                System.out.println ( " deplacement de 2 impossible vers une case de mer ");
     
                         }else{
                             if(this.joueur.getNombre_deplacement()>2)
                             {
                                 if (((posD.getNumero_ligne() == posA.getNumero_ligne()) && (y ==3 || y == -3)) || ((posD.getNumero_colone() == posA.getNumero_colone()) && (x == 3 || x == -3)) || ((y < 3 && y >-3) && (x == 3 || x == -3)) || ( (posD.getNumero_ligne()%2==0 && x<3 && x>-3 && x!=0 && y < 4 && y >-3 && y !=0 ) || (posD.getNumero_ligne()%2!=0 && x<4 && x>-4 && x!=0 && y < 4 && y >-4 && y !=0 )))                 
                                 {
-                                    this.deplacerPj(hexagoneDepart, hexagoneArrivee);
-                                    this.joueur.setNombre_deplacement(this.joueur.getNombre_deplacement() - 3);
-                                    deplacement= true;
-                                    if ( hexagoneArrivee.getTuile() == null)
-                                        this.est_nageur = true;
-        
+                                    if(hexagoneArrivee.getTuile() == null)
+                                    {
+                                        this.deplacerPj(hexagoneDepart, hexagoneArrivee);
+                                        this.joueur.setNombre_deplacement(this.joueur.getNombre_deplacement() - 3);
+                                        deplacement= true;
+                                        if ( hexagoneArrivee.getTuile() == null)
+                                            this.est_nageur = true;
+            
+                                    }else
+                                        System.out.println ( " deplacement de 3 impossible vers une case de mer ");
+                          
                                 }else{
                                     System.out.println("Deplacement impossible !!!!");
                                 }
